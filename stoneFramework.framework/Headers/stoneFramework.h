3c6ffdc4e49579a77507993d42b411f340ef45b0
Stone <wanglei@3k.com>
2021-12-01 16:16:29 +0800

    1

---
+++ stoneFramework.framework/Headers/stoneFramework.h
@@ -0,0 +1,18 @@
+//
+//  stoneFramework.h
+//  stoneFramework
+//
+//  Created by stone on 2021/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for stoneFramework.
+FOUNDATION_EXPORT double stoneFrameworkVersionNumber;
+
+//! Project version string for stoneFramework.
+FOUNDATION_EXPORT const unsigned char stoneFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <stoneFramework/PublicHeader.h>
+
+
